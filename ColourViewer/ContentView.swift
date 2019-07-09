31e6028d0a6853119b50f254167698d871cee35e
Michael Salmon <msalmonse@gmail.com>
2019-07-09 14:07:16 +0200

    Initial Commit

---
+++ ColourViewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ColourViewer
+//
+//  Created by Michael Salmon on 2019-07-09.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
