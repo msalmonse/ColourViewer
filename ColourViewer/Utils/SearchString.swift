a250f85832c9b99a0387d768ee643db6be54a5a5
Michael Salmon <msalmonse@gmail.com>
2019-07-31 20:46:01 +0200

    Added comment

--- ColourViewer/Utils/SearchString.swift
+++ ColourViewer/Utils/SearchString.swift
@@ -10,6 +10,11 @@ import Foundation
 import Combine
 
 class SearchString: Combine.ObservableObject, Identifiable {
+    /// A wrapper for String that can be used in SwiftUI
+    ///
+    /// As well as informing SwiftUI that it's value will change this class also publishes it's value
+    /// via publisher if the length of the string is greater than or equal to min
+    ///
     let id = UUID()
     let min = 2
     let publisher = PassthroughSubject<String, Never>()
