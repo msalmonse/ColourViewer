95ec3bacb4232b5631495c0852dc7e9078e3682e
Michael Salmon <msalmonse@gmail.com>
2019-08-01 13:38:04 +0200

    Added comments

--- ColourViewer/Models/NumberAndString.swift
+++ ColourViewer/Models/NumberAndString.swift
@@ -10,17 +10,23 @@ import Foundation
 import Combine
 import SwiftUI
 
+/// This class is a combined Int and String. Updating either will update the other provided that the values are valid
+
 class IntAndString: Combine.ObservableObject, Identifiable {
+    /// format and base are used for converting between Int and String
     private var format: String = ""
     private var base = 0
     
     let id = UUID()
+    // Allowable range for number
     let minNumber: Int
     let maxNumber: Int
 
+    // Notifiers, a publisher for SwiftUI and a callback
     var objectWillChange = ObservableObjectPublisher()
     var hasChanged: ((Int) -> ())? = nil
     
+    // The Int part, check for validity and update string
     var number: Int {
         willSet { objectWillChange.send() }
         didSet {
@@ -32,6 +38,7 @@ class IntAndString: Combine.ObservableObject, Identifiable {
         }
     }
     
+    // The string part, check for validity and update number
     var string: String = "" {
         willSet { objectWillChange.send() }
         didSet {
@@ -45,17 +52,27 @@ class IntAndString: Combine.ObservableObject, Identifiable {
         }
     }
     
+    // Check limits
     private func inRange(_ num: Int) -> Bool { num >= minNumber && num <= maxNumber }
     
+    // Format string
     private func formatString() {
         string = (format == "") ? "" : String(format: format, number)
     }
 
+    // Set the base and format according to the radix
     func setRadix(radix: Radix) {
         format = radix.format
         base = radix.base
         formatString()
     }
+    
+    /// Initializer
+    /// Parameters:
+    ///     number:     Initial value for number
+    ///     min:            Minimum limit
+    ///     max:           Maximum limit
+    ///     radix:          Initial radix for number
     
     init(number: Int, min: Int = Int.min, max: Int = Int.max, radix: Radix = .decimal) {
         self.number = number
@@ -70,20 +87,26 @@ class RGBandString: IntAndString {
     init(_ number: Int = 0) { super.init(number: number, min: 0, max: 255) }
 }
 
+/// This class is a combined Double and String, updating the one updates the other provided that the values are valid
+
 class DoubleAndString: Combine.ObservableObject, Identifiable {
     private var format: String = "%.2f"
     
     let id = UUID()
+    // Limits for value
     let minNumber: Double
     let maxNumber: Double
+    // Should the number wrap around if it is too big or small?
     var wrapMax = 0.0 {
         didSet { doWrap = true }
     }
     var doWrap = false
 
+    // Notifiers
     var objectWillChange = ObservableObjectPublisher()
     var hasChanged: ((Double) -> ())? = nil
     
+    // The number part
     var number: Double {
         willSet { objectWillChange.send() }
         didSet {
@@ -96,6 +119,7 @@ class DoubleAndString: Combine.ObservableObject, Identifiable {
         }
     }
     
+    // The string part
     var string: String = "" {
         willSet { objectWillChange.send() }
         didSet {
@@ -109,12 +133,15 @@ class DoubleAndString: Combine.ObservableObject, Identifiable {
         }
     }
     
+    // Check limits
     private func inRange(_ num: Double) -> Bool { num >= minNumber && num <= maxNumber }
     
+    // Format string
     private func formatString() {
         string = (format == "") ? "" : String(format: format, number)
     }
     
+    // Wrap the number around so that it is between min and wrapMax
     private func wrapNumber() {
         if inRange(number) || minNumber == -Double.infinity { return }
         var val = number - minNumber
@@ -122,6 +149,12 @@ class DoubleAndString: Combine.ObservableObject, Identifiable {
         if val < 0 { number = val + wrapMax}
         else { number = val + minNumber }
     }
+    
+    /// Initializer
+    /// Parameters:
+    ///     number:     Initial value for number
+    ///     min:            Minimum limit
+    ///     max:           Maximum limit
     
     init(number: Double, min: Double = -Double.infinity, max: Double = Double.infinity) {
         self.number = number
@@ -131,10 +164,12 @@ class DoubleAndString: Combine.ObservableObject, Identifiable {
     }
 }
 
+// For per cent values
 class PerCentAndString: DoubleAndString {
     init(percent: Double = 0.0) { super.init(number: percent, min: 0.0, max: 100.0)}
 }
 
+// For degrees
 class DegreesAndString: DoubleAndString {
     init(degrees: Double = 0.0) {
         super.init(number: degrees, min: 0.0, max: 359.99)
