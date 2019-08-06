bda4366b5e7711fb32dff87a1f1d1bee154753ee
Michael Salmon <msalmonse@gmail.com>
2019-08-06 08:54:08 +0200

    Added file to fetch error descriptions

---
+++ ColourViewer/Utils/ErrorText.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorText.swift
+//  ColourViewer
+//
+//  Created by Michael Salmon on 2019-08-06.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import Foundation
+
+/// Return the description of an error
+
+func errorText(_ err: Error) -> String {
+    return err.localizedDescription
+}
