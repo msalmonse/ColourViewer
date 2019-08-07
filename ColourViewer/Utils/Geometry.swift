83aded2868b6176ce454310dce8ac3d80ab5ae5b
Michael Salmon <msalmonse@gmail.com>
2019-08-07 15:14:08 +0200

    Added relativeSize to create new scaled CGSize

--- ColourViewer/Utils/Geometry.swift
+++ ColourViewer/Utils/Geometry.swift
@@ -10,28 +10,35 @@ import Foundation
 import SwiftUI
 
 extension GeometryProxy {
-    /// Calculate the relative width
+    // Calculate the relative width
     func relativeWidth(_ factor: Double) -> CGFloat {
         return self.size.relativeWidth(factor)
     }
 
-    /// Calculate the relative height
+    // Calculate the relative height
     func relativeHeight(_ factor: Double) -> CGFloat {
         return self.size.relativeHeight(factor)
     }
     
-    /// Is the display in landscape
+    // Is the display in landscape
     var isLandscape: Bool { return self.size.width > self.size.height }
 }
 
 extension CGSize {
-    /// Calculate the relative width
+    // Calculate the relative width
     func relativeWidth(_ factor: Double) -> CGFloat {
         return self.width * CGFloat(factor)
     }
 
-    /// Calculate the relative height
+    // Calculate the relative height
     func relativeHeight(_ factor: Double) -> CGFloat {
         return self.height * CGFloat(factor)
+    }
+    
+    // Combined relative width and height
+    func relativeSize(_ widthFactor: Double, _ heightFactor: Double) -> CGSize {
+        let w = self.relativeWidth(widthFactor)
+        let h = self.relativeHeight(heightFactor)
+        return CGSize(width: w, height: h)
     }
 }
